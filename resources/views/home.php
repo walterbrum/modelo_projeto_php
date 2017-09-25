f2163edb9c8628986bd8108a9dbfc57c2364285e
Walter Ferreira Brum Júnior <walterbrum@gmail.com>
2017-09-25 00:10:29 -0300

    Melhorias

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,3 +1,5 @@
-<div class="w3-content">
-	<h2>Dentro do Home</h2>
+<div class="w3-container">
+	<div class="w3-content">
+		<h2>Dentro do Home</h2>
+	</div>
 </div>
--- resources/views/template/layout.php
+++ resources/views/template/layout.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="/css/w3.css">
-	<link rel="shortcut icon" type="image/x-icon" href="/img/favicon.ico" />
+	<link rel="icon" type="image/png" sizes="96x96" href="/images/favicon.png">
 </head>
 <body>
 <header class="w3-container w3-indigo">
